18bc7af157dea13922ed6cd1491189bd5dfd86cf
stardust012 <161234026+stardust012@users.noreply.github.com>
2024-02-25 15:53:31 -0500

    initial take on setting up portfolio structure of

---
+++ src/pages/Commissions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Commissions() {
+    return (
+      <div>
+        <h1>Commissions</h1>
+        <p>Commission info</p>
+      </div>
+    );
+  }
+
+export default Commissions;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <p>Work, projects, or achievements.</p>
+        </div>
+    );
+}
+
+export default Portfolio;
